a9ebd005804985637e6baf6fc1852c811330d2b6
maylandertamas <maylandertamas@gmail.com>
2017-10-10 13:50:10 +0200

    Implemented solution

--- src/com/minespweeper/codecool/Main.java
+++ src/com/minespweeper/codecool/Main.java
@@ -5,16 +5,18 @@ import java.util.Random;
 
 public class Main {
 
-    public static int GenerateRandomNumBetween0And10() {
+    public static char mineField[][];
+
+    public static int GenerateRandomNumBetween0And11() {
         Random random = new Random();
-        return random.nextInt(10-0+1) + 0;
+        return random.nextInt(11-0+1) + 0;
     }
 
-    public static char[][] generateCharArray() {
+    public static void generateCharArray() {
         char[][] randomCharArray = new char[5][5];
         Arrays.stream(randomCharArray).forEach(w -> {
             for (int i = 0; i < w.length; i++) {
-                if (GenerateRandomNumBetween0And10() == 1) {
+                if (GenerateRandomNumBetween0And11() % 5 == 0) {
                     w[i] = '*';
                 } else {
                     w[i] = '.';
@@ -22,23 +24,55 @@ public class Main {
         }
         });
 
-        return randomCharArray;
+        mineField = randomCharArray;
+    }
+    public static int mineAt(int y, int x) {
+        if(y >= 0 && y < 5 && x >= 0 && x < 5 && mineField[y][x] == '*') {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 
-    public static char[][] transformCharArrays(char[][] generatedCharArray ) {
-        Arrays.stream(generatedCharArray).forEach(w -> {
-            if (generateCharArray()[-1][2] == 0) {
-                System.out.println("lol");
+    public static char minesNear(int y, int x) {
+        int mines = 0;
+        // check mines in all directions
+        mines += mineAt(y - 1, x - 1);
+        mines += mineAt(y - 1, x);
+        mines += mineAt(y - 1, x + 1);
+        mines += mineAt(y, x - 1);
+        mines += mineAt(y, x + 1);
+        mines += mineAt(y + 1, x - 1);
+        mines += mineAt(y + 1, x);
+        mines += mineAt(y + 1, x + 1);
+        if(mines > 0) {
+            return (char)(mines+ 48);
+        } else {
+            return '0';
+        }
+    }
+
+    public static void transformCharArrays(char[][] generatedCharArray ) {
+        for (int i = 0; i < generatedCharArray.length; i++) {
+            for (int k = 0; k < generatedCharArray[i].length; k++) {
+                if(mineField[i][k] != '*') {
+                    mineField[i][k] = minesNear(i, k);
+                }
             }
 
-        });
-        return new char[1][1];
+        }
     }
+
+    public static void printTables(char[][] table) {
+        Arrays.stream(table).forEach(t -> System.out.println(t));
+    }
+
 
     public static void main(String[] args) {
 
-
-        Arrays.stream(generateCharArray()).forEach(w -> System.out.println(w));
-        transformCharArrays(generateCharArray());
+        generateCharArray();
+        printTables(mineField);
+        transformCharArrays(mineField);
+        printTables(mineField);
     }
 }
